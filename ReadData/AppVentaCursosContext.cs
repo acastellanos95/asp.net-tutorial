81e870f00c33c9d25be95350dc558cff4161a5cd
Andre Castellanos <acastellanosaldama@gmail.com>
2020-12-21 15:40:10 -0600

    typo: Dbset to DbSet

--- ReadData/AppVentaCursosContext.cs
+++ ReadData/AppVentaCursosContext.cs
@@ -6,7 +6,7 @@ namespace ReadData
 	public class AppVentaCursosContext : DbContext
 	{
 		private const string connectionString = @"Data Source=localhost\SQLEXPRESS; Initial Catalog=EntityFWCore-2-5;Integrated Security=True";
-		public Dbset<Curso> Curso { get; set; }
+		public DbSet<Curso> Curso { get; set; }
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseSqlServer(connectionString);

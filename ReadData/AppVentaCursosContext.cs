853399aa63e4f5c2ca2d2b22b14554b55d239a27
Andre Castellanos <acastellanosaldama@gmail.com>
2020-12-21 15:12:55 -0600

    added DbSet of Cursos

---
+++ ReadData/AppVentaCursosContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace ReadData
+{
+	public class AppVentaCursosContext : DbContext
+	{
+		private const string connectionString = @"Data Source=localhost\SQLEXPRESS; Initial Catalog=EntityFWCore-2-5;Integrated Security=True";
+		public Dbset<Curso> Curso { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(connectionString);
+		}
+	}
+}
